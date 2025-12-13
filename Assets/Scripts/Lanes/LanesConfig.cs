1e78f94313b57e3117689245678f381cc830490e
Ivan Tutunaru <itutunaru1998@gmail.com>
2025-12-13 10:47:51 +0200

    Create basic lanes configuration

---
+++ Assets/Scripts/Lanes/LanesConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Frogxel.Lanes
+{
+    [CreateAssetMenu(fileName = "LanesConfig", menuName = "Lanes/New Lanes Config")]
+    public class LanesConfig : ScriptableObject
+    {
+        [field: SerializeField] public int LaneWidth { get; private set; }
+        [field: SerializeField] public List<LaneConfig> Lanes { get; private set; }
+    }
+}
---
+++ Assets/Scripts/Lanes/MovingLaneConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Frogxel.Lanes
+{
+    [CreateAssetMenu(fileName = "MovingLaneConfig", menuName = "Lanes/New Moving Lane Config")]
+    public class MovingLaneConfig : LaneConfig
+    {
+        [field: Header("Moving Lane")]
+        [field: SerializeField] public GameObject MoveablePrefab { get; private set; }
+        [field: SerializeField] public Vector2 MoveDirection { get; private set; }
+        [field: SerializeField] public float MoveSpeed { get; private set; }
+        [field: SerializeField] public float GapWidth { get; private set; }
+    }
+}
