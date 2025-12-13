beff461286beb47ffdf3b6262fa205480f457811
Ivan Tutunaru <itutunaru1998@gmail.com>
2025-12-13 10:49:37 +0200

    Create new lanes controller to generate lanes based on the configuration

---
+++ Assets/Scripts/Lanes/LanesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Frogxel.Lanes
+{
+    public class LanesController : MonoBehaviour
+    {
+        [SerializeField] private LanesConfig lanesConfig;
+        [SerializeField] private Transform lanesParent;
+        [SerializeField] private Vector2 startingLanePosition;
+        
+        private readonly List<LaneController> _lanes = new();
+        
+        private Vector2 _currentLanePosition;
+
+        public void Init()
+        {
+            _currentLanePosition = startingLanePosition;
+            
+            foreach (var config in lanesConfig.Lanes)
+            {
+                // TODO: Replace with object pooling
+                var lane = Instantiate(config.Prefab, _currentLanePosition + new Vector2(0, config.Height / 2f), Quaternion.identity, lanesParent);
+                
+                lane.Init(config, lanesConfig.LaneWidth);
+                
+                _lanes.Add(lane);
+                
+                _currentLanePosition += new Vector2(0, config.Height);
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (var lane in _lanes)
+            {
+                Destroy(lane.gameObject);
+            }
+            
+            _lanes.Clear();
+        }
+    }
+}
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Frogxel.Lanes;
 using UnityEngine;
 
 namespace Managers
@@ -8,8 +9,7 @@ namespace Managers
     /// </summary>
     public class GameManager : MonoBehaviour
     {
-        // Instance of the Lane Controller
-        [SerializeField] private LaneController laneController;
+        [SerializeField] private LanesController lanesController;
         
         [Header("Managers")]
         [SerializeField] private PlayerManager playerManager;
@@ -25,8 +25,7 @@ namespace Managers
         public void NewGame()
         {
             pauseManager.UnpauseGame();
-            
-            laneController.EndGame();
+            lanesController.Clear();
             
             // If there are players in the game
             if (playerManager.GetPlayers().Count > 0)
@@ -34,7 +33,7 @@ namespace Managers
                 scoreManager.ResetPlayerScore();
             }
 
-            laneController.FillLanes();
+            lanesController.Init();
 
             NewLevel();
         }
