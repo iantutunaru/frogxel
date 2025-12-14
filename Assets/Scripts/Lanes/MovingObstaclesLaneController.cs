101c1a76b7e833879a953bd712a8888922c9393c
Ivan Tutunaru <itutunaru1998@gmail.com>
2025-12-14 10:01:49 +0200

    Separate logic for obstacle groups into a new class

--- Assets/Scripts/Lanes/MovingObstaclesLaneController.cs
+++ Assets/Scripts/Lanes/MovingObstaclesLaneController.cs
@@ -8,7 +8,7 @@ namespace Frogxel.Lanes
         private const int ObstacleWidth = 1;
         
         [Header("Moving Lane")]
-        [SerializeField] private Obstacle obstaclePrefab;
+        [SerializeField] private ObstacleGroup obstacleGroupPrefab;
         
         public override void Init(LaneConfig laneConfig, int width)
         {
@@ -21,66 +21,33 @@ namespace Frogxel.Lanes
 
             CreateObstacles(movingLaneConfig, width);
         }
-        
-        private static float GetStartingObstaclePositionX(int totalObstacles)
-        {
-            if (totalObstacles <= 1)
-            {
-                return 0;
-            }
-
-            return 0 - ObstacleWidth / 2f * (totalObstacles - 1);
-        }
 
         private void CreateObstacles(MovingObstaclesLaneConfig movingObstaclesLaneConfig, int width)
         {
-            var totalObstaclesPerContainer = movingObstaclesLaneConfig.Count;
+            var totalObstaclesPerGroup = movingObstaclesLaneConfig.Count;
             var gapWidth = movingObstaclesLaneConfig.GapWidth;
-            var obstacleContainerWidth = totalObstaclesPerContainer * ObstacleWidth;
-            var totalSpacePerObstacleContainer = obstacleContainerWidth + gapWidth;
+            var obstacleGroupWidth = totalObstaclesPerGroup * ObstacleWidth;
+            var totalSpacePerObstacleGroup = obstacleGroupWidth + gapWidth;
 
-            if (width < totalSpacePerObstacleContainer)
+            if (width < totalSpacePerObstacleGroup)
             {
-                throw new Exception($"Not enough space to create obstacles. Need at least {totalSpacePerObstacleContainer}, but only have {width}");
+                throw new Exception($"Not enough space to create obstacles. Need at least {totalSpacePerObstacleGroup}, but only have {width}");
             }
             
-            var obstaclesCount = Mathf.CeilToInt(width * 1f / totalSpacePerObstacleContainer);
-            var obstacleContainerPositionX = 0 - width / 2f + obstacleContainerWidth / 2f;
-            var spaceBetweenObstacleContainers = obstacleContainerWidth + gapWidth;
+            var obstaclesCount = Mathf.CeilToInt(width * 1f / totalSpacePerObstacleGroup);
+            var obstacleGroupPositionX = 0 - width / 2f + obstacleGroupWidth / 2f;
+            var spaceBetweenObstacleGroups = obstacleGroupWidth + gapWidth;
             
             for (var i = 0; i < obstaclesCount; i++)
             {
-                var obstacleContainer = new GameObject($"ObstacleContainer_{i}");
-                var obstacleContainerTransform = obstacleContainer.transform;
-
-                obstacleContainerTransform.SetParent(transform, false);
+                var obstacleGroup = Instantiate(obstacleGroupPrefab, transform, false);
                 
-                obstacleContainerTransform.localPosition = new Vector3(obstacleContainerPositionX, 0, 0);
-
-                obstacleContainerPositionX += spaceBetweenObstacleContainers;
+                obstacleGroup.transform.localPosition = new Vector3(obstacleGroupPositionX, 0, 0);
                 
-                var obstaclePositionX = GetStartingObstaclePositionX(totalObstaclesPerContainer);
+                obstacleGroupPositionX += spaceBetweenObstacleGroups;
 
-                for (var j = 0; j < totalObstaclesPerContainer; j++)
-                {
-                    var obstacle = CreateObstacle(movingObstaclesLaneConfig, obstacleContainerTransform);
-                    
-                    obstacle.transform.localPosition = new Vector3(obstaclePositionX, 0, 0);
-                    
-                    obstaclePositionX += ObstacleWidth;
-                }
+                obstacleGroup.Init(movingObstaclesLaneConfig);
             }
-        }
-
-        private Obstacle CreateObstacle(MovingObstaclesLaneConfig movingObstaclesLaneConfig, Transform parent)
-        {
-            // TODO: Replace with object pooling
-            var obstacle = Instantiate(obstaclePrefab, parent);
-            
-            obstacle.SetSprite(movingObstaclesLaneConfig.Sprite);
-            obstacle.TrySetAnimatorController(movingObstaclesLaneConfig.AnimatorController);
-
-            return obstacle;
         }
     }
 }
---
+++ Assets/Scripts/Lanes/ObstacleGroup.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Frogxel.Lanes
+{
+    public class ObstacleGroup : MonoBehaviour
+    {
+        private const int ObstacleWidth = 1;
+        
+        [SerializeField] private Obstacle obstaclePrefab;
+
+        private readonly List<Obstacle> _obstacles = new();
+
+        public void Init(MovingObstaclesLaneConfig movingObstaclesLaneConfig)
+        {
+            var totalObstacles = movingObstaclesLaneConfig.Count;
+            var obstaclePositionX = GetStartingObstaclePositionX(totalObstacles);
+
+            for (var i = 0; i < totalObstacles; i++)
+            {
+                var obstacle = CreateObstacle(movingObstaclesLaneConfig, transform);
+                    
+                obstacle.transform.localPosition = new Vector3(obstaclePositionX, 0, 0);
+                    
+                obstaclePositionX += ObstacleWidth;
+                
+                _obstacles.Add(obstacle);
+            }
+        }
+        
+        private static float GetStartingObstaclePositionX(int totalObstacles)
+        {
+            if (totalObstacles <= 1)
+            {
+                return 0;
+            }
+
+            return 0 - ObstacleWidth / 2f * (totalObstacles - 1);
+        }
+        
+        private Obstacle CreateObstacle(MovingObstaclesLaneConfig movingObstaclesLaneConfig, Transform parent)
+        {
+            // TODO: Replace with object pooling
+            var obstacle = Instantiate(obstaclePrefab, parent);
+            
+            obstacle.SetSprite(movingObstaclesLaneConfig.Sprite);
+            obstacle.TrySetAnimatorController(movingObstaclesLaneConfig.AnimatorController);
+
+            return obstacle;
+        }
+    }
+}
