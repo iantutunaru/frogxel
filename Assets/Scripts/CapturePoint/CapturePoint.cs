a1e3be1beba287a810c784d634ad2409a53c2415
Ivan Tutunaru <itutunaru1998@gmail.com>
2025-12-14 19:03:06 +0200

    Convert capture points to a custom lane

---
+++ Assets/Scripts/CapturePoint/CapturePoint.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Class that handles the behavior of the capture points in their interactions with the player
+/// </summary>
+public class CapturePoint : MonoBehaviour
+{
+    public static event Action<PlayerController> Captured;
+    
+    [SerializeField] private GameObject trophy;
+    [SerializeField] private string playerTag;
+    
+    public bool IsCaptured { get; private set; }
+    
+    public void Clear()
+    {
+        IsCaptured = false;
+        
+        trophy.SetActive(false);
+    }
+
+    /// <summary>
+    /// Handles interaction with a player
+    /// </summary>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // if player touches the home base then inform Game Manager
+        if (!other.CompareTag(playerTag))
+        {
+            return;
+        }
+
+        var player = other.GetComponent<PlayerController>();
+
+        if (player == null)
+        {
+            throw new Exception($"\"{other.gameObject.name}\" has \"{playerTag}\" but is not a player and tried to capture a point \"{name}\"");
+        }
+
+        TryCapture(player);
+    }
+
+    private void TryCapture(PlayerController player)
+    {
+        if (IsCaptured)
+        {
+            return;
+        }
+        
+        IsCaptured = true;
+        
+        trophy.SetActive(true);
+        
+        Captured?.Invoke(player);
+    }
+}
