efaa0a1ef6c44956966810f6180f5a63278526ff
Ivan Tutunaru <itutunaru1998@gmail.com>
2025-12-14 14:21:17 +0200

    Fix moving obstacles lane config header

--- Assets/Scripts/Lanes/MovingObstaclesLaneConfig.cs
+++ Assets/Scripts/Lanes/MovingObstaclesLaneConfig.cs
@@ -6,7 +6,7 @@ namespace Frogxel.Lanes
     [CreateAssetMenu(fileName = "MovingObstaclesLane", menuName = "Lanes/New Moving Obstacles Lane Config")]
     public class MovingObstaclesLaneConfig : MovingLaneConfig
     {
-        [Header("Moving Obstacles Lane")]
+        [field: Header("Moving Obstacles Lane")]
         [field: SerializeField] public Sprite Sprite { get; private set; }
         [field: SerializeField] public AnimatorController AnimatorController { get; private set; }
         [field: SerializeField] public int Count { get; private set; }
