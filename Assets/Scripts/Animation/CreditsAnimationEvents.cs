6417503156a202dcd7d0585779b48f80955bad2a
Ivan Tutunaru <itutunaru1998@gmail.com>
2025-12-22 11:26:03 +0200

    Return to the main menu after credits are finished

---
+++ Assets/Scripts/Animation/CreditsAnimationEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Frogxel.Animation
+{
+    public class CreditsAnimationEvents : MonoBehaviour
+    {
+        public event Action CreditsEnd;
+        
+        public void HandleCreditsEnd()
+        {
+            CreditsEnd?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Levels/CreditsLevel.cs
+++ Assets/Scripts/Levels/CreditsLevel.cs
@@ -1,7 +1,31 @@
-﻿namespace Frogxel.Levels
+﻿using Frogxel.Animation;
+using Frogxel.Managers;
+using UnityEngine;
+
+namespace Frogxel.Levels
 {
     public class CreditsLevel : Level
     {
+        [SerializeField] private CreditsAnimationEvents animationEvents;
+        [SerializeField] private string mainMenuSceneName;
         
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            
+            animationEvents.CreditsEnd += HandleCreditsEnd;
+        }
+
+        public override void OnExit()
+        {
+            animationEvents.CreditsEnd -= HandleCreditsEnd;
+            
+            base.OnExit();
+        }
+        
+        private void HandleCreditsEnd()
+        {
+            LevelManager.TryChangeLevel(mainMenuSceneName);
+        }
     }
 }
